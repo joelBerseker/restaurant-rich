97a1f24ed26430559d28d988d28e687602988350
joelBersek <jatamaria@unsa.edu.pe>
2023-09-17 23:08:17 -0500

    a

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,7 +4,7 @@
     <div class="row justify-content-center">
       <div class="col-lg-6">
         <img
-          src="/restaurant-rich/about.jpg"
+          src="/public/about.jpg"
           alt="Imagen del Restaurante"
           class="img-fluid rounded-circle mb-4"
         />
